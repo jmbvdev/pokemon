84031c0550884fdaaef7847db9cf9c6aa0aaf17b
jmbvdev <jmvb.dev@gmail.com>
2022-03-29 13:16:12 -0400

    fix

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "../ProgressBar/progressBar.css"
 import PokemonsColors from '../PokemonsColors';
 const ProgressBar = ({data,type}) => {
+    if (data>=100) {
+        data=100
+    }
     return (
         <div className='progress' >
             <div className='progress-done' style={{opacity: 1,width:`${data}%`, background:`${PokemonsColors(type)}`}}>
